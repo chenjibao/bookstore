0f58fa3e35099711171266daf3a65a3387a41e27
chenjibao <17327364980@163.com>
2018-03-04 21:27:11 +0800

    创建UserDao,UserService,UserServlet类

---
+++ src/cjb/bookstore/user/service/UserService.java
@@ -0,0 +1,7 @@
+package cjb.bookstore.user.service;
+
+import cjb.bookstore.user.dao.UserDao;
+
+public class UserService {
+	private UserDao userDao=new UserDao();
+}
---
+++ src/cjb/bookstore/user/web/servlet/UserServlet.java
@@ -0,0 +1,8 @@
+package cjb.bookstore.user.web.servlet;
+
+import cjb.bookstore.user.service.UserService;
+import cn.itcast.servlet.BaseServlet;
+
+public class UserServlet extends BaseServlet {
+	private UserService userService=new UserService();
+}
